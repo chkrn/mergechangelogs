b3f38a12dc7caeda6734b35c15c509d1a4eba0c1
Sergey Volkhin <volkhin.sergey@globinform.ru>
2019-06-13 11:56:39 +0300

    Merging to files

--- mergechangelogs.py
+++ mergechangelogs.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def test():
+	chapters = {}
 	files = ("test/1/CHANGELOG.MD", "test/2/CHANGELOG.md")
 	for i in files:
 		with open(i) as file:
 			line = file.readline()
 			while line:
 				if line.startswith("##"):
-					print(">> " + line, end="")
+					chapter = chapters.get(line)
+					if not chapter:
+						chapter = []
+						chapters[line] = chapter
 				else:
-					print("> " + line, end="")
+					chapter.append(line)
+					
 				line = file.readline()
+
+	for chapter in sorted(chapters.keys()):
+		print(">> " + chapter, end = "")
+		for line in chapters[chapter]:
+			print("> " + line, end = "")
 
 if __name__ == "__main__":
 	test()
