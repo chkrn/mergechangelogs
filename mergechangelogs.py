efa4621c24546a32bc3a07cff04bb0ee71b16835
Sergey Volkhin <volkhin.sergey@globinform.ru>
2019-06-13 11:04:18 +0300

    Read one file and filter headers

--- mergechangelogs.py
+++ mergechangelogs.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def test():
-	print("Hi, World!")
+	with open('test/1/CHANGELOG.MD') as file:
+		line = file.readline()
+		while line:
+			if line.startswith("##"):
+				print(">> " + line, end="")
+			else:
+				print("> " + line, end="")
+			line = file.readline()
 
 if __name__ == "__main__":
 	test()
