3ea451dcb509f2cffa9ce1328aa310dc418b4a0a
Sergey Volkhin <volkhin.sergey@globinform.ru>
2019-06-13 11:21:27 +0300

    Read two files

--- mergechangelogs.py
+++ mergechangelogs.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 def test():
-	with open('test/1/CHANGELOG.MD') as file:
-		line = file.readline()
-		while line:
-			if line.startswith("##"):
-				print(">> " + line, end="")
-			else:
-				print("> " + line, end="")
+	files = ("test/1/CHANGELOG.MD", "test/2/CHANGELOG.md")
+	for i in files:
+		with open(i) as file:
 			line = file.readline()
+			while line:
+				if line.startswith("##"):
+					print(">> " + line, end="")
+				else:
+					print("> " + line, end="")
+				line = file.readline()
 
 if __name__ == "__main__":
 	test()
